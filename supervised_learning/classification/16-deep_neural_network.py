03d208c1a26a808aa35e245235e6e567d12326ae
Faithirakoze <f.irakoze2@alustudent.com>
2026-09-12 11:03:30 +0200

    DeepNeuralNetwork

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Defines a deep neural network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Defines a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """
+        Class constructor
+
+        nx is the number of input features
+        layers is a list representing the number of nodes in
+        each layer of the network
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(self.L):
+            if type(layers[i]) is not int or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+
+            if i == 0:
+                prev = nx
+            else:
+                prev = layers[i - 1]
+
+            self.weights["W" + str(i + 1)] = (
+                np.random.randn(layers[i], prev) * np.sqrt(2 / prev)
+            )
+            self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
