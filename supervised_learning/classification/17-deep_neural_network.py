933ec5c5bd969fd60d13ee4731f5833b209b513a
Faithirakoze <f.irakoze2@alustudent.com>
2026-09-12 12:33:48 +0200

    Privatize DeepNeuralNetwork

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Defines a deep neural network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Defines a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """
+        Class constructor
+
+        nx is the number of input features
+        layers is a list representing the number of nodes in
+        each layer of the network
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.__L):
+            if type(layers[i]) is not int or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+
+            if i == 0:
+                prev = nx
+            else:
+                prev = layers[i - 1]
+
+            self.__weights["W" + str(i + 1)] = (
+                np.random.randn(layers[i], prev) * np.sqrt(2 / prev)
+            )
+            self.__weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        """Getter for the number of layers"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """Getter for the cache dictionary"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """Getter for the weights dictionary"""
+        return self.__weights
